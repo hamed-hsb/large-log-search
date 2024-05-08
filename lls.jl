5050a73ae05cfe983fc1a8475476373af68fd6e9
kali <kali@kali.kali>
2024-05-08 21:37:42 +0330

    feat: read all files in a directory.

--- lls.jl
+++ lls.jl
@@ -1,7 +1,8 @@
 function __init__()
     currentDir()
-    readFile("/home/kali/Documents/logs/android logs/laravel-2024-04-20.log")
-   #listOfFilesInDir("/home/kali/Documents/logs/android logs/")
+    #readFile("/home/kali/Documents/logs/android logs/laravel-2024-04-20.log")
+    #listOfFilesInDir("/home/kali/Documents/logs/android logs/")
+    readAllFile("/home/kali/Documents/logs/android logs/test/")
 end
 
 
@@ -12,6 +13,7 @@ end
 
 
 function readFile(filePath)
+    println("file path : $filePath")
     open(filePath,"r") do f
         line = 0
         while ! eof(f)
@@ -19,22 +21,27 @@ function readFile(filePath)
             line += 1
             println("$line . $s")
         end
-    close(filePath)
+   
     end
 end
 
 
 function listOfFilesInDir(dir)
     foreach(readdir(dir)) do f
-        println("/objects",f)
+        println("File name: ",f)
        # dump(stat(f)) # you can customize what you want to print
     end
 end
 
 
-
-    
-
+function readAllFile(dir)
+    file_path = ""
+    foreach(readdir(dir)) do f
+        file_path = dir * "/" * f
+        readFile(file_path)
+       # dump(stat(f)) # you can customize what you want to print
+    end
+end
 
 
 
