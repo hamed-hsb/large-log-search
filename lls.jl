128283ac92260a0f3308b16ee6f6e71bff32434f
kali <h.safarzadeh2014@gmail.com>
2024-05-09 18:10:18 +0330

    feat: insert to file.

--- lls.jl
+++ lls.jl
@@ -5,21 +5,50 @@ ERR_MESS_IS_NOT_DIR = "no such file or directory"
 
 INFO_MESS_CREATE_FOLDER = "Result folder created successfully."
 
+INPUT_MESS_DIRECTORY_PATH = "Enter directory path:"
+INPUT_MESS_CHOOSE_READ_FILE = "Choose one of the following modes to read files: \n read all file in directory (0). \n read first file in directory (1). \n read last file in directory (2). \n Enter Number :"
+INPUT_MESS_CHOOSE_SAVE_FILTER = "Choose one of the following modes to read files: \n save (0). \n does not save (1).  \n Enter Number :"
 
 result_dir_name = "result"
 result_file_name = "result.log"
 
+save_file_state = "-1"
+read_file_state = "-1"
+
 function main()
+    __init__()
 end
 
 
 function __init__()
-    currentDir()
-    #readFile("/home/kali/Documents/logs/android logs/laravel-2024-04-20.log")
-    #listOfFilesInDir("/home/kali/Documents/logs/android logs/")
-    #readLastFile("/home/kali/Documents/logs/android logs/test/")
-    createFolder("/home/kali/Documents/logs/all logs/")
-    createLogFile("/home/kali/Documents/logs/all logs/")
+    println(INPUT_MESS_DIRECTORY_PATH)
+    dir_path = readInput()
+
+    println(INPUT_MESS_CHOOSE_READ_FILE)
+    global read_file_state = readInput()
+
+
+    println(INPUT_MESS_CHOOSE_SAVE_FILTER)
+    global save_file_state = readInput()
+
+  
+   
+ 
+    if save_file_state == "0"
+        println("/home/kali/Documents/logs/all logs")
+        createFolder(dir_path)
+        createLogFile(dir_path)
+    end 
+
+
+    if read_file_state == "0"
+        readAllFile(dir_path)
+    elseif read_file_state == "1"
+        readFirstFile(dir_path)
+    elseif read_file_state == "2"
+        readLastFile(dir_path)
+    end
+
 end
 
 
@@ -29,14 +58,20 @@ function currentDir()
 end
 
 
-function readFile(filePath)
+function readFile(filePath,pathDir)
     println("file path : $filePath")
+    file_path_result_log = pathDir * "/" *result_dir_name * "/" * result_file_name
     open(filePath,"r") do f
         line = 0
         while ! eof(f)
             s = readline(f) 
             line += 1
-            println("$line . $s")
+
+            println(save_file_state)
+            if save_file_state == "0"
+                insertToResultFileLog(file_path_result_log,s)
+            end
+        
         end
    
     end
@@ -55,7 +90,7 @@ function readAllFile(dir)
     file_path = ""
     foreach(readdir(dir)) do f
         file_path = dir * "/" * f
-        readFile(file_path)
+        readFile(file_path,dir)
        # dump(stat(f)) # you can customize what you want to print
     end
 end
@@ -63,14 +98,14 @@ end
 function readFirstFile(dir)
     file_name = first(readdir(dir))
     file_path = dir * "/" * file_name
-    readFile(file_path)
+    readFile(file_path,dir)
 end
 
 
 function readLastFile(dir)
     file_name = last(readdir(dir))
     file_path = dir * "/" * file_name
-    readFile(file_path)
+    readFile(file_path,dir)
    
 end
 
@@ -118,12 +153,19 @@ end
 
 
 function createLogFile(dir)
-    file = dir * "/" *result_dir_name * "/" * result_file_name
+    file =  dir * "/" *result_dir_name * "/" * result_file_name
     touch(file)
 end
 
 function readInput()
     return readline()
+end
+
+function insertToResultFileLog(filePath,text)
+    println("s s: $filePath")
+    file = open(filePath,"a")
+    write(file,text)
+  
 end
 
 
